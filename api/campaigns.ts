5c787c00dc91fe4dd4a024507f54374647f89a22
Envidicy <102771205+Sonic77734442@users.noreply.github.com>
2026-07-02 14:51:33 +0500

    Update campaigns.ts

--- api/campaigns.ts
+++ api/campaigns.ts
@@ -321,7 +321,7 @@ export default async function handler(req: any, res: any) {
 
 	cpc:
   	row.id === "1815ba00-98f0-4e95-9803-fbf759a3a0b9"
-    	? 0.08
+    	? 0.09
     	: clicks > 0
     	? Number((roundMoney(spendClientRaw) / clicks).toFixed(2))
     	: 0,
